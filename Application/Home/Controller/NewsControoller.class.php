da1318c62d614164ba395c35f40a8d9ab8010069
Fengalbean <yibin045@163.com>
2015-03-01 23:10:03 +0800

    add controller

---
+++ Application/Home/Controller/NewsControoller.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/3/1
+ * Time: 13:41
+ */
+namespace Home\Controller;
+use Think\Controller;
+class NewsController extends Controller {
+    public function index(){
+        echo 'NewsController';
+        $this->display();
+    }
+}
---
+++ Application/Home/Controller/Productcontroller.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/3/1
+ * Time: 13:46
+ */
+
+namespace Home\Controller;
+
+use Think\Controller;
+class ProductController extends Controller {
+    public function index(){
+        echo 'ProductController';
+        $this->display();
+    }
+}
